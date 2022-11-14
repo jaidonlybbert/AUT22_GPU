0296511e511fca9ec371c61fc1fda8761f44dfba
jaidonlybbert <jaidonlybbert@gmail.com>
2022-11-14 05:34:56 -0800

    Finished HW3, started 4

---
+++ HW3/submission/hw3_divergence.cu
@@ -0,0 +1,123 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "../Util/cuda_helpers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+cudaError_t executeCuda(int width);
+
+/*
+    3 KERNELS FOR TESTING EFFECTS OF DIVERGENCE
+*/
+
+__global__ void no_divergence(int* res, int width)
+{
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int temp = col;
+
+    if (width % 2 == 0) {
+        temp += row * 10;
+    }
+
+    __syncthreads();
+    
+    res[row * width + col] = temp;
+
+    return;
+}
+
+__global__ void single_branch_divergence(int *res, int width)
+{
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int temp = col;
+
+    if (temp % 2 == 0) {
+        temp += row * 10;
+    }
+    else {
+        temp += row * 1000;
+    }
+
+    __syncthreads();
+
+    res[row * width + col] = temp;
+    
+    return;
+}
+
+__global__ void nested_branch_divergence(int* res, int width)
+{
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int temp = col;
+
+    if (temp % 2 == 0) { // do for every other column
+        temp += row * 10;
+        if (temp % 3 == 0) { // do for every 6th column
+            temp += row*1000;
+        }
+    }
+
+    __syncthreads();
+
+    res[row * width + col] = temp;
+
+    return;
+}
+
+int main()
+{
+    int width = 36;
+
+    // Run kernel
+    checkCuda(executeCuda(width));
+
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+    checkCuda(cudaDeviceReset());
+
+    return 0;
+}
+
+cudaError_t executeCuda(int width)
+{
+    int *res = (int *)malloc(width*width*sizeof(int));
+    int *dev_res = 0;
+    cudaError_t cudaStatus;
+
+    // Choose which GPU to run on, change this on a multi-GPU system.
+    checkCuda(cudaSetDevice(0));
+    // Allocate GPU memory for variable
+    checkCuda(cudaMalloc((void**)&dev_res, width*width*sizeof(int)));
+    // Copy result from host memory to GPU buffers.
+    checkCuda(cudaMemcpy(dev_res, res, width*width*sizeof(int), cudaMemcpyHostToDevice));
+
+    // Launch a kernel on the GPU with one thread for each element.
+    dim3 DimGrid(23, 36, 1); dim3 DimBlock(16, 16, 1);
+    no_divergence<<<DimGrid, DimBlock >>>(dev_res, width);
+    // Check for any errors launching the kernel
+    checkCuda(cudaGetLastError());
+    // cudaDeviceSynchronize waits for the kernel to finish, and returns
+    // any errors encountered during the launch.
+    checkCuda(cudaDeviceSynchronize());
+
+    single_branch_divergence << <DimGrid, DimBlock >> > (dev_res, width);
+    checkCuda(cudaGetLastError());
+    checkCuda(cudaDeviceSynchronize());
+
+    nested_branch_divergence << <DimGrid, DimBlock >> > (dev_res, width);
+    checkCuda(cudaGetLastError());
+    checkCuda(cudaDeviceSynchronize());
+
+    // Copy output vector from GPU buffer to host memory.
+    checkCuda(cudaMemcpy(res, dev_res, width*width*sizeof(int), cudaMemcpyDeviceToHost));
+
+
+Error:
+    cudaFree(dev_res);
+    
+    return cudaStatus;
+}
---
+++ HW4/HW4/HW4/hw4.cu
@@ -0,0 +1,188 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "../Util/cuda_helpers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define BLOCK_HEIGHT 32
+#define BLOCK_WIDTH 8
+#define MATRX_SIZE 128
+
+float max_error(float* dev_result, float* host_result, int size);
+
+/*
+    3 MMULT VARIANTS
+*/
+void host_mmult(float* c_mtx, float* a_mtx, float* b_mtx, int a_width, int a_height, int b_width)
+/*
+* Computes result c matrix from the matrix multiplication C = AB with the CPU
+*
+* Assumed a_width == b_height
+*
+* Dimensions of C are a_height x b_width
+*/
+{
+    // For each position in the C matrix
+    for (int i = 0; i < a_height; i++) {
+        for (int j = 0; j < b_width; j++) {
+            // Calculate the inner product of the row of A and column of B
+            float innerProduct = 0;
+            for (int k = 0; k < a_width; k++) {
+                innerProduct += a_mtx[a_width * i + k] * b_mtx[b_width * k + j];
+            }
+            c_mtx[b_width * i + j] = innerProduct;
+        }
+    }
+}
+
+__global__ void global_mem_mmult(float* c_mtx, float* a_mtx, float* b_mtx, int a_width, int a_height, int b_width)
+/*
+* Computes result c matrix from the matrix multiplication C = AB using global memory with CUDA
+*
+* Assumed a_width == b_height
+*
+* Dimensions of C are a_height x b_width
+*/
+{
+    // row and column of the C result
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (row < a_height && col < b_width) {
+        // Calculate the inner product of the row of A and column of B
+        float innerProduct = 0;
+        for (int i = 0; i < a_width; i++) {
+            innerProduct += a_mtx[a_width * row + i] * b_mtx[b_width * i + col];
+        }
+
+        c_mtx[b_width * row + col] = innerProduct;
+    }
+}
+
+
+__global__ void shared_mem_mmult(float* c_mtx, float* a_mtx, float* b_mtx, int a_width, int a_height, int b_width)
+/*
+* Computes result c matrix from the matrix multiplication C = AB using shared memory with CUDA
+*
+* Assumed a_width == b_height
+*
+* Dimensions of C are a_height x b_width
+*/
+{
+    // row and column of C result
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    __shared__ float ads[BLOCK_HEIGHT][BLOCK_WIDTH];
+    __shared__ float bds[BLOCK_HEIGHT][BLOCK_WIDTH];
+
+    int ty = threadIdx.y, tx = threadIdx.x;
+
+    int phases = ceil(a_width / (float)BLOCK_WIDTH);
+
+    float pval = 0.0;
+    for (int i = 0; i < phases; i++) {
+        if ((i * BLOCK_WIDTH + tx < a_width) && (row < a_height)) {
+            ads[ty][tx] = a_mtx[row * a_width + i * BLOCK_WIDTH + tx];
+        }
+
+        if ((i * BLOCK_WIDTH + ty < a_width) && (col < b_width)) {
+            bds[ty][tx] = b_mtx[(i * BLOCK_WIDTH + ty) * b_width + col];
+        }
+
+        __syncthreads();
+
+        for (int k = 0; k < BLOCK_WIDTH; k++) {
+            if ((i * BLOCK_WIDTH + k) < a_width)
+                pval += ads[ty][k] * bds[k][tx];
+        }
+        __syncthreads();
+    }
+
+    if (col < b_width && row < a_height) {
+        c_mtx[row * b_width + col] = pval;
+    }
+}
+
+
+int main()
+{
+    const int A_HEIGHT = MATRX_SIZE, A_WIDTH = MATRX_SIZE, B_HEIGHT = MATRX_SIZE, B_WIDTH = MATRX_SIZE;
+
+    // Host variables
+    float* h_c = (float*)malloc(A_HEIGHT * B_WIDTH * sizeof(float));
+    float* h_a = (float*)malloc(A_WIDTH * A_HEIGHT * sizeof(float));
+    float* h_b = (float*)malloc(B_WIDTH * B_HEIGHT * sizeof(float));
+    float* h_verify_res = (float*)malloc(A_HEIGHT * B_WIDTH * sizeof(float));
+
+    // Seed randomizer to generate input data
+    srand((unsigned int)time(NULL));
+
+    // Initialize input arrays
+    for (int i = 0; i < A_WIDTH * A_HEIGHT; i++) { h_a[i] = ((float)rand() / (float)(RAND_MAX)); }
+    for (int i = 0; i < B_WIDTH * B_HEIGHT; i++) { h_b[i] = ((float)rand() / (float)(RAND_MAX)); }
+
+    // Device variables
+    checkCuda(cudaSetDevice(0));
+
+    float* dev_c = 0, * dev_a = 0, * dev_b = 0;
+
+    checkCuda(cudaMalloc((void**)&dev_c, A_HEIGHT * B_WIDTH * sizeof(float)));
+    checkCuda(cudaMalloc((void**)&dev_a, A_HEIGHT * A_WIDTH * sizeof(float)));
+    checkCuda(cudaMalloc((void**)&dev_b, B_HEIGHT * B_WIDTH * sizeof(float)));
+    checkCuda(cudaMemcpy(dev_c, h_c, A_HEIGHT * B_WIDTH * sizeof(float), cudaMemcpyHostToDevice));
+    checkCuda(cudaMemcpy(dev_a, h_a, A_HEIGHT * A_WIDTH * sizeof(float), cudaMemcpyHostToDevice));
+    checkCuda(cudaMemcpy(dev_b, h_b, B_HEIGHT * B_WIDTH * sizeof(float), cudaMemcpyHostToDevice));
+
+    // Setup grid and launch kernel
+    dim3 DimGrid(ceil(B_WIDTH / (float)BLOCK_WIDTH), ceil(A_HEIGHT / (float)BLOCK_HEIGHT), 1); dim3 DimBlock(BLOCK_WIDTH, BLOCK_HEIGHT, 1);
+    global_mem_mmult << <DimGrid, DimBlock >> > (dev_c, dev_a, dev_b, A_WIDTH, A_HEIGHT, B_WIDTH);
+    checkCuda(cudaGetLastError()); // Check for any errors launching the kernel
+    checkCuda(cudaDeviceSynchronize());
+
+    shared_mem_mmult<<<DimGrid, DimBlock>>>(dev_c, dev_a, dev_b, A_WIDTH, A_HEIGHT, B_WIDTH);
+    checkCuda(cudaGetLastError()); // Check for any errors launching the kernel
+    checkCuda(cudaDeviceSynchronize());
+
+    // Copy output vector from GPU buffer to host memory.
+    checkCuda(cudaMemcpy(h_c, dev_c, A_HEIGHT * B_WIDTH * sizeof(float), cudaMemcpyDeviceToHost));
+
+    // Compute host-side verification result
+    host_mmult(h_verify_res, h_a, h_b, A_WIDTH, A_HEIGHT, B_WIDTH);
+
+    // Compare results
+    float maxError = max_error(h_verify_res, h_c, A_HEIGHT * B_WIDTH);
+
+    if (maxError < 1E-6) {
+        printf("Host verification passed.\n");
+    }
+    else {
+        printf("Host verification failed.\n");
+    }
+
+    printf("Max error: %.3E\n", maxError);
+
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+    checkCuda(cudaDeviceReset());
+
+Error:
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+    cudaFree(dev_c);
+    return 0;
+}
+
+
+float max_error(float* dev_result, float* host_result, int size) {
+    float maxError = 0.0;
+    for (int i = 0; i < size; i++) {
+        if (fabs(host_result[i] - dev_result[i]) / host_result[i] > maxError) {
+            maxError = fabs((host_result[i] - dev_result[i])) / host_result[i];
+        }
+    }
+    return maxError;
+}
